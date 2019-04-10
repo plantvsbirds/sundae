9737faa02a460b9b3d7f6ceac724bdf50d412a7b
pvb <plantvsbird@gmail.com>
2019-04-09 20:42:43 -0500

    state machine, DELETE

--- server/api.js
+++ server/api.js
@@ -6,9 +6,18 @@ const expressBodyParser = require('body-parser')
 innerApp.use(expressBodyParser.json())
 outerApp.use(expressBodyParser.json())
 
+const outerAppLongPoll = require("express-longpoll")(outerApp)
+
+const uuid = require('uuid/v1')
 
 const cookieStore = {}
-
+const instanceStages = [
+  'CREATE',
+  'RUNNING',
+  'KILLING',
+  'DEAD'
+]
+let instances = []
 
 innerApp.get('/ping', (req, res, next) => {
   res.json({'pong': true})
@@ -24,14 +33,41 @@ innerApp.post('/cookie-report', (req, res, next) => {
 
 innerApp.listen(12345, '127.0.0.1')
 
+const pub = (ins) => 
+    outerAppLongPoll.publish('/' + ins.id, ins)
+const createInstance = ({ payload }) => {
+  let ins = {
+    ...payload,
+    id: uuid(),
+    stage: instanceStages[0],
+  }
+  instances.push(ins)
+  outerAppLongPoll.create('/' + ins.id, {})
+  ins.poll = setInterval(() => {
+    pub(ins)
+  }, 1000)
+  return {id: ins.id}
+}
+const killInstance = ({ payload: { id } }) => {
+  let ins = instances.find((i) => i.id == id)
+  ins.stage = 'KILLING'
+
+  pub(ins)
+  setTimeout(() => {
+    clearInterval(ins.poll)
+  }, 60 * 1000)
+  return {id: ins.id}
+}
 // outerApp.use(express.static(__dirname + '/static'));
 outerApp.get('/share', (req, res, next) => {
   res.sendFile('./static/share.html', {root: '.'})
 })
 
 outerApp.post('/share', (req, res, next) => {
-  console.log(req.body)
-  res.json({'pong': true})
+  res.json(createInstance({ payload: req.body }))
+})
+outerApp.delete('/share', (req, res, next) => {
+  res.json(killInstance({ payload: req.body }))
 })
 
 
